4140ffd7815c0ac49836d521fa9707bc01635769
Abhishekanjana <abhishekanjana85@gmail.com>
2026-02-13 15:39:29 +0530

    working

--- shared/data/utilities/api.ts
+++ shared/data/utilities/api.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'https://addon-api.theodin.in/v1'; 
+// export const API_BASE_URL = 'https://addon-api.theodin.in/v1'; 
 export const RService_BASE_URL = 'https://addons.theodin.in/api/v1';
-//export const API_BASE_URL = 'http://localhost:8000/v1';
+export const API_BASE_URL = 'http://localhost:3003/v1';
  
